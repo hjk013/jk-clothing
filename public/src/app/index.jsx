1d0624a583b21ae53c9971e1676fdb1ed3de9ab4
Hyeon Jun Kim <hjk013@gmail.com>
2019-07-30 16:59:38 -0700

    implemented firebase utils, included ability to store authenticated users into firebase db

--- public/src/app/index.jsx
+++ public/src/app/index.jsx
@@ -38,6 +38,8 @@ class App extends React.Component {
             () => console.log('this.state', this.state)
           );
         });
+
+        console.log('this.state', this.state);
       } else {
         this.setState({ currentUser: userAuth });
       }
