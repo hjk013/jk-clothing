be62f2d7b44fe0a12c4edccfcffb5cbe7e08db6d
Hyeon Jun Kim <hjk013@gmail.com>
2019-09-01 02:10:24 -0700

    debugging api request fail

--- rest-server/server.js
+++ rest-server/server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 1212;
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public/dist')));
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '../public/dist')));
+}
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
