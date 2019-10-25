4d1ff205ec5f6d700ca29858c0ee901964733f17
Hyeon Jun Kim <hjk013@gmail.com>
2019-10-25 00:05:41 -0700

    added lazy suspense to homepage

--- public/src/pages/homepage/homepage.component.jsx
+++ public/src/pages/homepage/homepage.component.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Link } from 'react-router-dom';
 
 import './homepage.styles.scss';
-import Directory from '../../components/directory/directory.component';
-import title from '../../../dist/assets/hp_title.png';
+// import Directory from '../../components/directory/directory.component';
+// import title from '../../../dist/assets/hp_title.png';
 
 import { HomePageContainer } from './homepage.styles';
 
+const Directory = lazy(() =>
+  import('../../components/directory/directory.component')
+);
+const title = lazy(() => import('../../../dist/assets/hp_title.png'));
+
 const HomePage = () => (
-  <HomePageContainer>
-    <div className="home-bg">
-      <img className="home-title" src={title} alt="title"></img>
-      <Link className="home-button" to="/shop">
-        <h1 className="button-title">SHOP NOW</h1>
-      </Link>
-    </div>
-    <Directory />
-  </HomePageContainer>
+  <Suspense fallback={<div>Loading...</div>}>
+    <HomePageContainer>
+      <div className="home-bg">
+        <img className="home-title" src={title} alt="title"></img>
+        <Link className="home-button" to="/shop">
+          <h1 className="button-title">SHOP NOW</h1>
+        </Link>
+      </div>
+      <Directory />
+    </HomePageContainer>
+  </Suspense>
 );
 
 export default HomePage;
